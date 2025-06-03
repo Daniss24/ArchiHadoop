b14c2178894781923f497cd5359a6333305dbbf2
Daniss24 <akuelou2020@gmail.com>
2025-06-03 14:44:38 +0200

    Firstcommit

--- etl/config.py
+++ etl/config.py
@@ -3,5 +3,5 @@ MONGO_PORT = 27018
 MONGODB_URI = "mongodb://localhost:27018/"
 DB_NAME = "ionis_belib_db"
 COLLECTION_NAME = "bornes_belib"
-API_BASE_URL = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/belib-points-de-recharge-pour-vehicules-electriques-disponibilite-temps-reelss/records"
+API_BASE_URL = "https://opendata.paris.fr/api/explore/v2.1/catalog/datasets/belib-points-de-recharge-pour-vehicules-electriques-disponibilite-temps-reel/records"
 API_LIMIT = 100
